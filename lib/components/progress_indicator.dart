e430393d5e492ff8875d4eac17fb57b4caeeb5b2
Dennis Stumm <dennisstumm95@gmail.com>
2022-05-14 18:20:26 +0200

    Optimized progress indicator

--- lib/components/progress_indicator.dart
+++ lib/components/progress_indicator.dart
@@ -5,19 +5,8 @@ showProgressIndicator(BuildContext context) {
     barrierDismissible: false,
     context: context,
     builder: (BuildContext context) {
-      return AlertDialog(
-        content: Builder(
-          builder: (context) {
-            var height = MediaQuery.of(context).size.height;
-            var width = MediaQuery.of(context).size.width;
-
-            return SizedBox(
-              height: height - 400,
-              width: width - 400,
-              child: const CircularProgressIndicator()
-            );
-          }
-        )
+      return const Center(
+        child: CircularProgressIndicator(),
       );
     },
   );
