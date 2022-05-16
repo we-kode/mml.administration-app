cec45bcdbaa762470c8417f1aa3cbf442fb1c333
stummk <stummk@users.noreply.github.com>
2022-05-16 19:11:06 +0200

    New lines added

--- lib/views/users.dart
+++ lib/views/users.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mml_admin/view_models/main.dart';
-import 'package:provider/provider.dart';
 
 class UsersScreen extends StatelessWidget {
   const UsersScreen({Key? key}) : super(key: key);
