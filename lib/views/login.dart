6ff20bf9d98579be2ad9f6f0eb018bafe677d635
stummk <stummk@users.noreply.github.com>
2022-05-21 16:36:39 +0200

    Added expandtile to lgon screen
    Clsoes #8

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -38,17 +38,14 @@ class LoginScreen extends StatelessWidget {
                 return Container();
               }
 
-              return Form(
-                key: vm.formKey,
-                child: Center(
-                  child: ConstrainedBox(
-                    constraints: const BoxConstraints(
-                      minWidth: 400,
-                      minHeight: 250,
-                      maxWidth: 400,
-                      maxHeight: 700,
-                    ),
+              return Center(
+                child: Form(
+                  key: vm.formKey,
+                  child: SizedBox(
+                    width: 500,
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Image.asset(
                           'assets/images/logo_admin.png',
@@ -83,51 +80,62 @@ class LoginScreen extends StatelessWidget {
                           validator: vm.validatePassword,
                         ),
                         spacer,
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: vm.clientIdLabel,
-                            icon: const Icon(Icons.credit_score),
-                          ),
-                          onSaved: (String? clientId) {
-                            vm.clientId = clientId;
-                          },
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          validator: vm.validateClientId,
-                          onChanged: (String? clientId) {
-                            vm.clientId = clientId;
-                          },
+                        ExpansionTile(
+                          leading: const Icon(Icons.extension),
+                          initiallyExpanded: vm.showAdvanced,
+                          maintainState: true,
+                          title: Text(vm.locales.advanced),
+                          children: [
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: vm.clientIdLabel,
+                                icon: const Icon(Icons.credit_score),
+                              ),
+                              onSaved: (String? clientId) {
+                                vm.clientId = clientId;
+                              },
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              validator: vm.validateClientId,
+                              onChanged: (String? clientId) {
+                                vm.clientId = clientId;
+                              },
+                            ),
+                            spacer,
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: vm.serverNameLabel,
+                                icon: const Icon(Icons.storage),
+                              ),
+                              onSaved: (String? serverName) {
+                                vm.serverName = serverName;
+                              },
+                              validator: vm.validateServerName,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              onChanged: (String? serverName) {
+                                vm.serverName = serverName;
+                              },
+                            ),
+                            spacer,
+                            TextFormField(
+                              decoration: InputDecoration(
+                                labelText: vm.appKeyLabel,
+                                icon: const Icon(Icons.vpn_key_outlined),
+                              ),
+                              onSaved: (String? appKey) {
+                                vm.appKey = appKey;
+                              },
+                              validator: vm.validateAppKey,
+                              autovalidateMode:
+                                  AutovalidateMode.onUserInteraction,
+                              onChanged: (String? appKey) {
+                                vm.appKey = appKey;
+                              },
+                            ),
+                            spacer,
+                          ],
                         ),
-                        spacer,
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: vm.serverNameLabel,
-                            icon: const Icon(Icons.storage),
-                          ),
-                          onSaved: (String? serverName) {
-                            vm.serverName = serverName;
-                          },
-                          validator: vm.validateServerName,
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          onChanged: (String? serverName) {
-                            vm.serverName = serverName;
-                          },
-                        ),
-                        spacer,
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: vm.appKeyLabel,
-                            icon: const Icon(Icons.vpn_key_outlined),
-                          ),
-                          onSaved: (String? appKey) {
-                            vm.appKey = appKey;
-                          },
-                          validator: vm.validateAppKey,
-                          autovalidateMode: AutovalidateMode.onUserInteraction,
-                          onChanged: (String? appKey) {
-                            vm.appKey = appKey;
-                          },
-                        ),
-                        spacer,
                         spacer,
                         Align(
                           alignment: Alignment.centerRight,
